b052cdaf72c3ae0c1627f4fad273c615b32699b4
zhangnan24 <zhangnan35@foxmail.com>
2020-07-05 08:24:35 +0000

    Deploying to gh-pages from  @ 9efdfc0f006848ddb37585a55288423277ee3f3a 🚀

--- service-worker.js
+++ service-worker.js
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/page_demo/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
